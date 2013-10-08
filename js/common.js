cdb7efa3f9d41a7fde8d4ed0dea28cd56f1a4047
hraboviyvadim <hraboviyvadim@gmail.com>
2013-10-08 15:34:28 +0300

    registration added

--- js/common.js
+++ js/common.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
+	$(".datepicker").each(function(){
+		$(this).datepicker({
+			dateFormat: "dd.mm.yy",
+			minDate: new Date(2013, 10 - 1, 14),
+			maxDate: new Date(2013, 12 - 1, 8), 
+			onSelect: function(value) {
+            	$(".js-date").val(value);
+        	}
+		});
+	});
+	
+	$(".js-datepicker").click(function(){
+		$(".datepicker").toggleClass('is-open');
+	});
 
+	$(".hint span").click(function(){
+		$(".hint__bill").toggleClass('is-active');
+	});
 
 });
